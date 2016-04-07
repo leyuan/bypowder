2a9b8c0c589065c19d399feac1d8d0a822be54b6
Leyuan <leyuan.ly@outlook.com>
2016-04-06 21:19:45 -0600

    GA

--- public/js/main.js
+++ public/js/main.js
@@ -264,13 +264,6 @@ jQuery(document).on('ready', function(){
 	})(jQuery);
 });
 
-// jQuery(window).on('scroll', function(){
-// 	(function ($) {
-
-// 		stickyHeader();
-
-// 	})(jQuery);
-// });
 jQuery(window).on('load', function(){
 	(function ($) {
 
@@ -278,3 +271,12 @@ jQuery(window).on('load', function(){
 
 	})(jQuery);
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-76093812-1', 'auto');
+ga('send', 'pageview');
+
